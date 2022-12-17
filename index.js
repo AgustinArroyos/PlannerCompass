8acebc06ce9f50b6462a70d354cc598dc38aa465
AgustinArroyos <arroyosagustin@gmail.com>
2022-12-17 19:03:06 -0300

    implemetn with netfli

--- index.js
+++ index.js
@@ -1,23 +1,4 @@
-import express from "express";
-import conectDB from "./config/db.js";
-import dotenv from "dotenv";
-import userrouter from "./routes/userRoutes.js";
-import eventrouter from "./routes/eventRoutes.js";
-import eventdrouter from "./routes/eventDeletR.js";
-
-
-const app = express();
-dotenv.config();
-conectDB();
-app.use(express.json())
-
-app.use('/api/v1/users', userrouter)
-
-app.use('/api/v1/events', eventrouter)
-
-app.use('/api/v1/event', eventdrouter)
-
-
+import app from './app.js'
 
 const PORT = process.env.PORT || 4000
 app.listen(PORT, () => {
