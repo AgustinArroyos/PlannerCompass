c55c93911c8c067ddf42af93aad739d71586d66f
AgustinArroyos <arroyosagustin@gmail.com>
2022-12-17 20:35:34 -0300

    implemetn with netfli

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import app from './app.js'
-
+import serverless from 'serverless-http';
 const PORT = process.env.PORT || 4000
 app.listen(PORT, () => {
     console.log(`Server in ${PORT}`)
 })
+
+export const handler = serverless(app);
